f16e42e530eb311846183c8aef8d99897306064c
Daniel Nguyen <long@podzim.co>
2024-03-07 00:24:49 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwiftAnthropic",
     platforms: [
          .iOS(.v15),
-         .macOS(.v14)
+         .macOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
